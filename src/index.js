4451777f904f8954902c0af07192ff94e9837fc3
Eric Weissman <eric.weissman21@gmail.com>
2019-03-21 15:39:44 -0600

    Adds redux for index.js
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.scss';
 import App from './containers/App/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootReducer } from '../src/reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
