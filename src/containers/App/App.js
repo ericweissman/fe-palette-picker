4800986a951276384df0ea587ccf5da230c1783a
Eric Weissman <eric.weissman21@gmail.com>
2019-03-21 15:41:28 -0600

    Connects store to App initially
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../../main.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
       </div>
     );
   }
 }
+export const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
+  error: state.error
+});
 
-export default App;
+export default connect(mapStateToProps)(App);
+
+App.propTypes = {
+  error: PropTypes.string,
+  isLoading: PropTypes.bool,
+}
+
+App.defaultProps = {
+  error: '',
+  isLoading: true,
+}
