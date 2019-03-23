ae028df410f10c2c42c0cc0ae25cc3e051bf1666
Eric Weissman <eric.weissman21@gmail.com>
2019-03-23 11:07:17 -0600

    Adds base functionality to pull in palettes
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { getProjects } from '../../thunks/getProjects'
 import { addProject } from '../../thunks/addProject'
 import { deleteProject } from '../../thunks/deleteProject'
+import { getPalettes } from '../../thunks/getPalettes'
 import PropTypes from 'prop-types';
 import '../../main.scss';
 
@@ -39,6 +40,12 @@ class App extends Component {
     this.props.deleteProject(url, id)
   }
 
+  getPalettes = () => {
+    const id = this.props.projects[0].id
+    const url = process.env.REACT_APP_BACKEND_URL + `/api/v1/projects/${id}/palettes`
+    this.props.getPalettes(url)
+  }
+
   render() {
     return (
       <div className="App">
@@ -49,6 +56,7 @@ class App extends Component {
         </form>
         <div>
           <button onClick={this.delete}>Delete</button>
+          <button onClick={this.getPalettes}>Palettes</button>
         </div>
       </div>
     );
@@ -60,12 +68,14 @@ export const mapStateToProps = (state) => ({
   isLoading: state.isLoading,
   error: state.error,
   projects: state.projects,
+  palettes: state.palettes,
 });
 
 export const mapDispatchToProps = (dispatch) => ({
   getProjects: (url) => dispatch(getProjects(url)),
   addProject: (url, project) => dispatch(addProject(url, project)),
-  deleteProject: (url, projectID) => dispatch(deleteProject(url, projectID))
+  deleteProject: (url, projectID) => dispatch(deleteProject(url, projectID)),
+  getPalettes: (url) => dispatch(getPalettes(url)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
