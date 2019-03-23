3feb53df046291890794176400cc849fdb66dd35
Eric Weissman <eric.weissman21@gmail.com>
2019-03-23 10:13:20 -0600

    Adds action for posting to BE
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -2,6 +2,8 @@ export const projectsReducer = (state = [], action) => {
   switch(action.type) {
     case 'GET_PROJECTS_SUCCESS':
       return action.projects
+    case 'ADD_PROJECT_SUCCESS':
+      return [...state, action.project]
     default:
       return state
   }
