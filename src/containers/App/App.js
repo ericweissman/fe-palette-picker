a12968d6a837f6e8fd5bcfa08d8c4e8a042c43f9
Eric Weissman <eric.weissman21@gmail.com>
2019-03-23 10:13:34 -0600

    Adds basic functionality for adding project
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getProjects } from '../../thunks/getProjects'
+import { addProject } from '../../thunks/addProject'
 import PropTypes from 'prop-types';
 import '../../main.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projectName: '',
+    }
+  }
 
   componentDidMount() {
     const url = process.env.REACT_APP_BACKEND_URL + '/api/v1/projects'
     this.props.getProjects(url)
   }
   
+  handleClick = (e) => {
+    e.preventDefault();
+    const url = process.env.REACT_APP_BACKEND_URL + '/api/v1/projects'
+    this.props.addProject(url, this.state.projectName)
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Palette Picker</h1>
+        <form>
+          <input onChange={this.handleChange} placeholder='new-project-name' name='projectName' value={this.state.projectName}></input>
+          <button onClick={this.handleClick}>Save</button>
+        </form>
       </div>
     );
   }
@@ -28,7 +51,8 @@ export const mapStateToProps = (state) => ({
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  getProjects: (url) => dispatch(getProjects(url))
+  getProjects: (url) => dispatch(getProjects(url)),
+  addProject: (url, project) => dispatch(addProject(url, project))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
