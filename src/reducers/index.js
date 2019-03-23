e2f5bed1c0fe87f419e7f99dbc68ab0e94d248f8
Eric Weissman <eric.weissman21@gmail.com>
2019-03-23 09:44:41 -0600

    Adds project reducer
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { isLoadingReducer } from './isLoadingReducer'
 import { hasErroredReducer } from './hasErroredReducer'
+import { projectsReducer } from './projectsReducer'
 
 export const rootReducer = combineReducers({
   isLoading: isLoadingReducer,
   error: hasErroredReducer,
+  projects: projectsReducer,
 })
---
+++ src/reducers/projectsReducer.js
@@ -0,0 +1,13 @@
+export const projectsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_PROJECTS_SUCCESS':
+      return action.projects
+    default:
+      return state
+  }
+} 
+
+//addprojectsuccess
+
+//editprojectsuccess
+
