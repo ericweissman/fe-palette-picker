b8f88d562807d06a50e691916879b80c2b9b4ae9
Eric Weissman <eric.weissman21@gmail.com>
2019-03-26 17:21:45 -0600

    Makes Palette a class component
    Co-authored-by: Travis Gee <geet084@gmail.com>

--- src/containers/Palette/Palette.js
+++ src/containers/Palette/Palette.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+class Palette extends Component {
 
-const Palette = (props) => {
-  const { deletePalette, palette, setActive } = props
-  const colors = [palette.color_1, palette.color_2, palette.color_3, palette.color_4, palette.color_5]
+  handleEdit = () => {
+    const { palette } = this.props
+    const colors = [palette.color_1, palette.color_2, palette.color_3, palette.color_4, palette.color_5]
+    this.props.setActive(colors)
+    this.props.editPalette({ palette_name: palette.palette_name, id: palette.id, project_id: palette.project_id })
+  }
 
-  return (
-    <div className='palette'>
-      <h5 onClick={() => setActive(colors)}>{palette.palette_name}</h5>
-      <div className='palette-small'>
-        {
-          colors.map((color, i) => {
-            return (
-              <div key={i} onClick={() => setActive(colors)} className='small-color' style={{ backgroundColor: color }}> {color} </div>
-            )
-          })
-        }
-      </div >
-      <button onClick={() => deletePalette(palette.id)}>X</button>
-    </div>
-  )
+
+  render() {
+    const { deletePalette, palette, setActive } = this.props
+    const colors = [palette.color_1, palette.color_2, palette.color_3, palette.color_4, palette.color_5]
+
+    return (
+      <div className='palette'>
+        <h5 onClick={() => setActive(colors)}>{palette.palette_name}</h5>
+        <div className='palette-small'>
+          {
+            colors.map((color, i) => {
+              return (
+                <div key={i} onClick={() => setActive(colors)} className='small-color' style={{ backgroundColor: color }}> {color} </div>
+              )
+            })
+          }
+        </div >
+        <button onClick={() => deletePalette(palette.id)}>X</button>
+        <button onClick={this.handleEdit}> Edit</button>
+      </div>
+    )
+  }
+
 }
 
 export default Palette;
