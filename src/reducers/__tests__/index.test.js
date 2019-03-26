fb28ad3c0b0b6d24249a193f9e0f8c80f594e544
Travis Gee <geet084@gmail.com>
2019-03-26 08:43:48 -0600

    Adds initial tests for the root reducer

---
+++ src/reducers/__tests__/index.test.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux';
+import { rootReducer } from '../index';
+
+describe('rootReducer', () => {
+  let store = createStore(rootReducer)
+
+  it('should set the store with an initial state', () => {
+    let expected = {
+      activePalette: [],
+      projects: [],
+      palettes: [],
+      isLoading: true,
+      error: '',
+    }
+
+    expect(store.getState()).toEqual(expected)
+  })
+
+  it('should dispatch isLoading action', () => {
+    let action = { type: 'IS_LOADING', isLoading: false }
+    let expected = false;
+
+    store.dispatch(action)
+    expect(store.getState().isLoading).toEqual(expected)
+  })
+
+  it('should dispatch error action', () => {
+    let action = { type: 'HAS_ERRORED', message: 'test' }
+    let expected = 'test';
+
+    store.dispatch(action)
+    expect(store.getState().error).toEqual(expected)
+  })
+})
