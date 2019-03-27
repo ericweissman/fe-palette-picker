70f17bdb529f52415bb171e54b8384ad1ba8cf0b
Eric Weissman <eric.weissman21@gmail.com>
2019-03-27 08:59:07 -0600

    Adds tests for App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App, mapDispatchToProps } from './App';
+import { shallow, mount } from 'enzyme'
+import { handleProject } from '../../thunks/handleProject'
+import { mapStateToProps } from './App';
+import { getProjectsSuccess } from '../../actions'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+jest.mock('../../thunks/handleProject')
+describe('App', () => {
+  let wrapper
+  let mockProps = {
+    isLoading: false,
+    error: '',
+    handleProject: jest.fn()
+  }
+
+  it('should match the correct snapshot', () => {
+    wrapper = shallow(<App {...mockProps} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match the correct snapshot if loading is true', () => {
+    wrapper.setProps({ isLoading: true })
+    wrapper = shallow(<App {...mockProps} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return an object with isLoading and error', () => {
+      const mockState = {
+        isLoading: 'false',
+        error: '',
+        activePalette: {},
+        projects: [],
+        fakeState: [],
+        fakeState2: 21
+      }
+      const expected = {
+        isLoading: 'false',
+        error: '',
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with handleProject when componentDidMount is called', () => {
+      const mockDispatch = jest.fn()
+      const mockURL = 'http//localhost:1234'
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = handleProject(mockURL, getProjectsSuccess, 'GET')
+
+      mappedProps.handleProject(mockURL)
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
+  })
+})
