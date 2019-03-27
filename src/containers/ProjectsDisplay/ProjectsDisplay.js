c5ca802a3931aa0eefaf55ba8995e136be9ceb0d
Eric Weissman <eric.weissman21@gmail.com>
2019-03-27 17:19:21 -0600

    Adds ProjectsDisplay with Masonry

--- src/containers/ProjectsDisplay/ProjectsDisplay.js
+++ src/containers/ProjectsDisplay/ProjectsDisplay.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handlePalette } from '../../thunks/handlePalette';
 import ProjectCard from '../ProjectCard/ProjectCard'
+import Masonry from 'react-masonry-component'
+
 
 export class ProjectsDisplay extends Component {
 
   render() {
     const { projects } = this.props
-    
+    const breakpointColumnsObj = {
+      default: 4,
+      1100: 3,
+      700: 2,
+      500: 1
+    };
+
     return (
-      <div className="projects-container">
+      <Masonry 
+      breakpointCols={breakpointColumnsObj}
+      className="projects-display-grid"
+      columnClassName="projects-display-grid_column"
+      >
         {
           projects && projects.map(project => {
             return <ProjectCard key={project.id} project={project} />
           })
         }
-      </div>
+      </Masonry>
     )
   }
 }
